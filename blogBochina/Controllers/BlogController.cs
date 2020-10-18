87f6d0155fbca75299fc892f0076ab92f32c0319
Женя <tsparou_e@mail.ru>
2020-10-18 20:37:56 +0400

    Lab 5 ready

--- blogBochina/Controllers/BlogController.cs
+++ blogBochina/Controllers/BlogController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using blogBochina.Domain.DB;
+using blogBochina.Model.Common;
+using blogBochina.ViewModels.Account;
+using BlogForStudents.Security.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blogBochina.Controllers
@@ -11,9 +16,67 @@ namespace blogBochina.Controllers
     /// </summary>
     public class BlogController : Controller
     {
+
+        private readonly BlogDbContext _blogDbContext;
+
+        public BlogController(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext ?? throw new ArgumentNullException(nameof(blogDbContext));
+        }
+
+        /// <summary>
         /// Метод возращающий страницу блога
+        /// </summary>
         public IActionResult Index()
         {
+            IQueryable<BlogPostItemViewModel> posts = _blogDbContext.BlogPosts.Select(x => new BlogPostItemViewModel
+            {
+                Author = x.Owner.FullName,
+                Created = x.Created.ToString(),
+                Data = x.Data,
+                Title = x.Title
+            }).OrderByDescending(x => x.Created);
+            List<string> postings = new List<string>();
+            foreach (BlogPostItemViewModel post in posts) {
+                postings.Add(post.Author);
+                postings.Add(post.Created);
+                postings.Add(post.Title);
+                postings.Add(post.Data);
+            }                    
+            return View(postings);
+        }
+
+        /// <summary>
+        /// Метод открывает страницу с добавлением поста
+        /// </summary>
+        /// <returns>Возврпщает страницу для добавления поста</returns>
+       // [Authorize]
+       // [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public IActionResult AddPost(NewPostViewModel model)
+        //public IActionResult AddPost()
+        {
+            
+            
+            if (!ModelState.IsValid) return View(model);
+            //if (!ModelState.IsValid) return View();
+
+            var user = this.GetAuthorizedUser();
+            
+            var post = new BlogPost
+            {
+                Created = DateTime.Now,
+                Data = model.Data,
+                Title = model.Title,
+               //Data = "qwreqwe" ,
+                //Title = "gdfhfdgjhfdg",
+                Owner = user.Employee
+            };
+
+            _blogDbContext.BlogPosts.Add(post);
+
+            _blogDbContext.SaveChanges();
+            
             return View();
         }
     }
---
+++ blogBochina/ViewModels/Account/BlogPostItemViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blogBochina.ViewModels.Account
+{
+    /// <summary>
+    /// Добавляем новый пост
+    /// </summary>
+    public class BlogPostItemViewModel
+    {
+        /// <summary>
+        /// Автор поста
+        /// </summary>
+        [Required]
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Заголовок поста
+        /// </summary>
+        [Required]
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Текст поста
+        /// </summary>
+        [Required]
+        [Display(Name = "Текст")]
+        public string Data { get; set; }
+
+        /// <summary>
+        /// Время оформления поста
+        /// </summary>
+        [Required]
+        [Display(Name = "Время оформления")]
+        public string Created { get; set; }
+
+       
+    }
+}
